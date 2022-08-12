6a11aace369d0f883ebdb2afea7edc8f1ff24e08
DEIA'A ALSHAWISH <daalshawish20@cit.just.edu.jo>
2022-08-12 15:37:15 +0300

    add home page deiaa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChatModule } from './chat/chat.module';
+import { HomeModule } from './home/home.module';
 
 const routes: Routes = [
   {
     path:"Chat",
   loadChildren:()=>ChatModule
    },
+   {
+    path:"",
+  loadChildren:()=>HomeModule
+   }
 ];
 
 @NgModule({

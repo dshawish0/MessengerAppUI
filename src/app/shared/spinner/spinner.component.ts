89fbd57c4897f56d2a5106b77eba41f560412b30
DEIA'A ALSHAWISH <daalshawish20@cit.just.edu.jo>
2022-09-05 07:26:09 +0300

    Deiaa

---
+++ src/app/shared/spinner/spinner.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spinner',
+  templateUrl: './spinner.component.html',
+  styleUrls: ['./spinner.component.css']
+})
+export class SpinnerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    let spinner: any = document.querySelector(".spinner-bg-color") as HTMLElement;
+    spinner.classList.add("display-none");
+  }
+
+  
+
+   show() {
+    let spinner: any = document.querySelector(".spinner-bg-color") as HTMLElement;
+    spinner.classList.remove("display-none");
+  }
+
+   hide() {
+    let spinner: any = document.querySelector(".spinner-bg-color") as HTMLElement;
+    spinner.classList.add("display-none");
+  }
+
+
+}
